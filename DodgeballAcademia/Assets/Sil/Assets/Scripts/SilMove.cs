6ab6e1505e7e09e2f46516b477d8b5d21fd00ad5
Masenkyo <33390@ma-web.nl>
2022-11-23 10:22:25 +0100

    both movement

--- DodgeballAcademia/Assets/Sil/Assets/Scripts/SilMove.cs
+++ DodgeballAcademia/Assets/Sil/Assets/Scripts/SilMove.cs
@@ -4,42 +4,62 @@ using UnityEngine;
 
 public class SilMove : MonoBehaviour
 {
-    Rigidbody player;
     [SerializeField] float Speed;
     [SerializeField] float Multiplier;
+    [SerializeField] float Height;
     [SerializeField] float Tijd;
+    [SerializeField] bool isJumping;
+    [SerializeField] bool isJumpingDown;
 
     // Start is called before the first frame update
     void Start()
     {
-       player = GameObject.Find("Player").GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)){
-            gameObject.transform.position = new Vector3(transform.position.x - Speed * Time.deltaTime, transform.position.y, transform.position.z);
-        }
+        if (Input.GetKey(KeyCode.A) && gameObject.name == "Player1"){gameObject.transform.position = new Vector3(transform.position.x - Speed * Time.deltaTime, transform.position.y, transform.position.z);}
+        if (Input.GetKey(KeyCode.LeftArrow) && gameObject.name == "Player2"){gameObject.transform.position = new Vector3(transform.position.x - Speed * Time.deltaTime, transform.position.y, transform.position.z);}
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && gameObject.name == "Player1"){gameObject.transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime, transform.position.y, transform.position.z);}
+        if (Input.GetKey(KeyCode.RightArrow) && gameObject.name == "Player2"){gameObject.transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime, transform.position.y, transform.position.z);}
+
+        if (Input.GetKey(KeyCode.S) && gameObject.name == "Player1"){gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - Speed * Time.deltaTime);}
+        if (Input.GetKey(KeyCode.DownArrow) && gameObject.name == "Player2"){gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - Speed * Time.deltaTime);}
+
+
+        if (Input.GetKey(KeyCode.W) && gameObject.name == "Player1")
         {
-            gameObject.transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime, transform.position.y, transform.position.z);
+            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Speed * Time.deltaTime);
         }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - Speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow) && gameObject.name == "Player2")
         {
             gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Speed * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isJumping == true)
         {
-            player.AddForce(transform.position.x, Speed * 100 * Multiplier, transform.position.z);
+            Tijd += Time.deltaTime + Time.deltaTime + Time.deltaTime + Time.deltaTime + Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, Mathf.Sin(Tijd) * Height, transform.position.z);
+            if (Tijd >= 3.2f)
+            {
+                Tijd = 0;
+                isJumping = false;
+                transform.position = new Vector3(transform.position.x, Mathf.Sin(Tijd) * Height, transform.position.z);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && gameObject.name == "Player1")
+            {
+                isJumping = true;
+            }
+            if (Input.GetKeyDown(KeyCode.RightShift) && gameObject.name == "Player2")
+            {
+                isJumping = true;
+            }
         }
     }
 }
