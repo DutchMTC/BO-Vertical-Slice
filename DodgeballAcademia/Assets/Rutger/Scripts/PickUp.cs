031d3fea5ded3678549d734207d9597dc0df4c06
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2022-11-23 11:22:26 +0100

    pickup progress

--- DodgeballAcademia/Assets/Rutger/Scripts/PickUp.cs
+++ DodgeballAcademia/Assets/Rutger/Scripts/PickUp.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     public bool isBall = true;
+
+    public List<bool> CanPickUp;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,15 @@ public class PickUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject.Find("Player1").GetComponent<RutMove>().Distance1 = Vector3.Distance(gameObject.transform.position, GameObject.Find("Player1").transform.position);
+        GameObject.Find("Player2").GetComponent<RutMove>().Distance1 = Vector3.Distance(gameObject.transform.position, GameObject.Find("Player2").transform.position);
+        if (GameObject.Find("Player2").GetComponent<RutMove>().Distance1 >= 1)
+        {
+            CanPickUp[0] = true;
+        }
+        if (Distance2 >= 1)
+        {
+            CanPickUp[1] = true;
+        }
     }
 }
