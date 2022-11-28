acc85fced391aa9cf92275ee1da9acd305070597
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2022-11-28 09:20:57 +0100

    pushi

--- DodgeballAcademia/Assets/Rutger/Scripts/Throw.cs
+++ DodgeballAcademia/Assets/Rutger/Scripts/Throw.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Catch : MonoBehaviour
+public class Throw : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
