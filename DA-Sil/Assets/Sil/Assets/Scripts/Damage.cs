05cc7f535a1a8e910204fb3bf9f51350163b0aea
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2022-12-11 22:07:06 +0100

    damaga

---
+++ DA-Sil/Assets/Sil/Assets/Scripts/Damage.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField] float damage;
+    
+
+    void Update()
+    {
+        if (gameObject.GetComponent<PickUp>().Distance1 <= 1)
+        {
+            if (gameObject.GetComponent<PickUp>().isPickedUp == false && gameObject.GetComponent<PickUp>().thrownByLeft == false)
+            {
+                if (GetComponent<BallMove>().isInAir == true)
+                {
+                    GameObject.Find("MovementController").GetComponent<FollowTheLeader>().leader1.GetComponent<Health>().TakeDamage(damage);
+                    GetComponent<BallMove>().isInAir = false;
+                    Debug.Log(GetComponent<PickUp>().Distance1);
+                }
+            }
+        }
+        if (gameObject.GetComponent<PickUp>().Distance4 <= 1)
+        {
+            if (gameObject.GetComponent<PickUp>().isPickedUp == false && gameObject.GetComponent<PickUp>().thrownByLeft == true)
+            {
+                if (GetComponent<BallMove>().isInAir == true)
+                {
+                    GameObject.Find("MovementController").GetComponent<FollowTheLeader>().leader2.GetComponent<Health>().TakeDamage(damage);
+                    GetComponent<BallMove>().isInAir = false;
+                }                
+            }
+        }
+    }
+}
--- DA-Sil/Assets/Sil/Assets/Scripts/Throw.cs
+++ DA-Sil/Assets/Sil/Assets/Scripts/Throw.cs
@@ -20,6 +20,8 @@ public class Throw : MonoBehaviour
             {
                 GameObject Thrown = Instantiate(ball);
                 Thrown.GetComponent<BallMove>().thrower = 1;
+                Thrown.GetComponent<BallMove>().isInAir = true;
+                Thrown.GetComponent<PickUp>().thrownByLeft = true;
                 PickedUp -= 1;
             }
         }
@@ -30,6 +32,8 @@ public class Throw : MonoBehaviour
             {
                 GameObject Thrown = Instantiate(ball);
                 Thrown.GetComponent<BallMove>().thrower = 2;
+                Thrown.GetComponent<BallMove>().isInAir = true;
+                Thrown.GetComponent<PickUp>().thrownByLeft = false;
                 PickedUp -= 1;
             }
         }
